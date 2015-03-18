48100a8ca39877c6aebbfce1cb5d686c697079ae
RemyPortier <remy.portier@aquafadas.com>
2015-03-18 14:34:27 +0100

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -95,7 +95,7 @@ default['nginx']['multi_accept']         = false
 default['nginx']['event']                = nil
 default['nginx']['server_tokens']        = nil
 default['nginx']['server_names_hash_bucket_size'] = 64
-default['nginx']['sendfile'] = 'on'
+default['nginx']['sendfile'] = 'off'
 
 default['nginx']['access_log_options']     = nil
 default['nginx']['error_log_options']      = nil
